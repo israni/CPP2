e1b184b0318d519192e1c333769f3be0e3868db8
israni <israni@umich.edu>
2019-05-26 08:28:25 -0400

    Added multidimensional arrays to arrays.cpp

--- src/arrays.cpp
+++ src/arrays.cpp
@@ -34,5 +34,23 @@ int main()
         cout << "\t" << my_array_3[i] << " ";
     }
 
+
+    cout << endl << endl << "4. Multidimensional Array" << endl;
+    // Multidimensional arrays are similar to arrays.
+    int multi_myarray[5][5] = { {1,2,3,4,5},
+                                {1,2,3,4,5},
+                                {1,2,3,4,5},
+                                {1,2,3,4,5},
+                                {1,2,3,4,5} };
+    
+    // Print multidimesnional array
+    for (int i=0;i<5; i++)
+    {
+        for (int j=0; j<5; j++)
+        {
+            cout << multi_myarray[i][j] << " ";
+        }
+        cout << endl;
+    }
     return(0);
 }
