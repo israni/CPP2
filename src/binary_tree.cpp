5a0ee3846a5c4205195ae4d5389632a015d2b822
Sagar Israni <sagar.israni@traxen.ai>
2021-04-04 19:29:58 -0400

    Added small example of deep/shallow copy

--- src/binary_tree.cpp
+++ src/binary_tree.cpp
@@ -12,6 +12,21 @@ int main()
     else{
         std::cout << "NOT EQUAL" << std::endl;    
     }
-    std::cout << mynode1 << std::endl;
+    std::cout << "mynode1: " << mynode1 << std::endl;
+    
+    // The data attribute is deep copied as it is defined in the stack at compile time.
+    node mynode3(30);
+    std::cout << "mynode3: " << mynode3 << std::endl;
+
+    mynode3 = mynode1;
+    std::cout << "mynode3: " << mynode3 << std::endl;
+
+    mynode3.set_data(50);
+    std::cout << "mynode3: " << mynode3 << std::endl;
+    std::cout << "mynode1: " << mynode1 << std::endl;
+
+    // Need to show that the pointers left,right would not be deep copied as they would be defined in the heap.
+
     std::cout << "Done" << std::endl;
+
 }
--- src/node.h
+++ src/node.h
@@ -3,36 +3,44 @@
 class node{
     private:
         int data;
-        struct node* left; // Pointer to left node
-        struct node* right; // Pointer to right node
+        node* left; // Pointer to left node
+        node* right; // Pointer to right node
     public:
+        // node();
         node(int val);
+        // ~node();
         int get_data(); 
         void set_data(int val); 
-        bool operator==(const node& other_node);  
+        bool operator==(const node& other_node); 
+        // Friend functions are not member functions but can access the private and public members of the class. 
         friend std::ostream& operator<<(std::ostream& os, const node& n);
 };
 
-std::ostream& operator<<(std::ostream& os, const node& n)
-{
-    os << n.data;
-    return os;
-}
-
+// Constructor for node class
 node::node(int val){
     data = val;
     left = nullptr;
     right = nullptr;
 }
 
+// Getter function
 int node::get_data(){
     return(data);
 }
 
+// Setter function
 void node::set_data(int val){
     data = val;
 }
 
+// Overloaded comparison operator to compare two nodes
 bool node::operator==(const node &other_node){
     return other_node.data == data;
+}
+
+// Overloaded print operator to print the value of node to ostream
+std::ostream& operator<<(std::ostream& os, const node& n)
+{
+    os << n.data;
+    return os;
 }
